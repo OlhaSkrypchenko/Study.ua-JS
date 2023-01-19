bf13ae9c487e2398a9b6d47e4a5f01ef7a52a71e
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-01-19 17:05:39 +0200

    added all tasks resolutions

--- script.js
+++ script.js
@@ -22,3 +22,51 @@ if (userAge) {
 } else {
   alert("User did not provide required data");
 }
+
+// task2
+
+let displayProperty = prompt("Enter one of display property like: block, flex, grid, inline or none");
+
+switch(displayProperty) {
+    case 'block': alert(displayProperty);
+    break;
+    case 'flex': alert(displayProperty);
+    break;
+    case 'grid': alert(displayProperty);
+    break;
+    case 'inline': alert(displayProperty);
+    break;
+    case 'none': alert(displayProperty);
+    break;
+    default: alert("Set 'display:inline-block;'");
+}
+
+// task 3
+
+let num = prompt("Please enter the number", 1);
+let multiplyNum = 12;
+let exitMessage = "User did not provide required data";
+
+if (num !== null) {
+  while (isNaN(num) === true || num <= 0) {
+    num = prompt("Your number is not correct, please enter correct number");
+
+    if (num === null) {
+      alert(exitMessage);
+    }
+  }
+
+  if (num > 0) {
+    alert(`${num} by ${multiplyNum} is ${num * multiplyNum}`);
+  }
+} else {
+  alert(exitMessage);
+}
+
+// task 4
+
+for (let i = 2; i <= 50; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
