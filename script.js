f72468e181cce16cfc4f885c61cd4a7c1566af5c
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-01-19 12:16:55 +0200

    handle cancelation of the programm

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ let basicSalaryAmount = 2000;
 
 let firstMonthSalary = prompt("Enter first month salary", defaultFirstValue);
 let secondMonthSalary = prompt("Enter second month salary", defaultSecondValue);
-let totalSalaryAmount = +firstMonthSalary + +secondMonthSalary;
-let summarySalaryAmount = totalSalaryAmount + bonus;
 
-
-let message = alert(`Your salary for 2 months is: ${totalSalaryAmount}$`);
-let bonusMessage = alert(`You have extra bonus!Your total salary was: ${totalSalaryAmount}$. It's increase for ${bonus}$ and now it is ${summarySalaryAmount}$`);
-
-// task3
-// let containWork = alert(`Will you work next? ${summarySalaryAmount >= basicSalaryAmount}`);
-
-let containWork = alert(`${summarySalaryAmount >= basicSalaryAmount ? 'I\'m ready to work' : 'I\'m out'}`);
+if (firstMonthSalary && secondMonthSalary) {
+    let totalSalaryAmount = Number(firstMonthSalary) + Number(secondMonthSalary);
+    let summarySalaryAmount = totalSalaryAmount + bonus;
+    alert(`Your salary for 2 months is: ${totalSalaryAmount}$`);
+    alert(`You have extra bonus!Your total salary was: ${totalSalaryAmount}$. It's increase for ${bonus}$ and now it is ${summarySalaryAmount}$`);
+    // task3
+    // alert(`Will you work next? ${summarySalaryAmount >= basicSalaryAmount}`);
+    alert(`${summarySalaryAmount >= basicSalaryAmount ? 'I\'m ready to work' : 'I\'m out'}`);
+} else {
+    alert('User did not provide required data');
+}
