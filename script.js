0b764c0811d300780a5008dfb1ad2943a67f864e
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-01-19 19:30:21 +0200

    final

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ console.log(`You can ignore single and double quotes like this: \\' \\' \\" \\"`
 // task2
 let enterData=confirm('Please enter your data');
 
-let userName = prompt('Enter your name');
+let userName = prompt('Enter your name', 'Name');
 let userNameInfo = alert(`Your name is: ${userName}`);
 
-let userSurname = prompt('Enter your surname');
+let userSurname = prompt('Enter your surname', 'Surname');
 let userFullNameInfo = alert(`Your full name is: ${userName} ${userSurname}`);
 
-let userAge = prompt('How old are you?');
+let userAge = prompt('How old are you?', 'Age');
 let basicUserInfo = alert(`Your full name is: ${userName} ${userSurname}, your age: ${userAge}`);
 
 let isAdmin = confirm('Are you admin?');
