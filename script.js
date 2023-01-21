ec33f1b28e3b00f80a1869bc78e87ce08aa85d58
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-01-21 13:43:33 +0200

    added solutions for all tasks

--- script.js
+++ script.js
@@ -4,42 +4,42 @@ const errorMessage = "User did not provide required data";
 
 //task 1
 
-let a = prompt('Enter first number');
-let b = prompt('Enter first second');
+let a = prompt("Enter first number");
+let b = prompt("Enter first second");
 
- function maxNumber(a, b){
-    if (a === null || b === null) {
-        return errorMessage;
-    }
+function maxNumber(a, b) {
+  if (a === null || b === null) {
+    return errorMessage;
+  }
 
-    let firstNum = Number(a);
-    let secondNum = Number(b);
+  let firstNum = Number(a);
+  let secondNum = Number(b);
 
-    if (isNaN(firstNum) || isNaN(secondNum)) {
-        return errorMessage;
-    }
+  if (isNaN(firstNum) || isNaN(secondNum)) {
+    return errorMessage;
+  }
 
-    if (firstNum === secondNum) {
-        return "a = b";
-    }
+  if (firstNum === secondNum) {
+    return "a = b";
+  }
 
-    return firstNum > secondNum ? firstNum : secondNum;
- }
+  return firstNum > secondNum ? firstNum : secondNum;
+}
 
- alert(maxNumber(a, b));
+alert(maxNumber(a, b));
 
 //task 2
 
- function reverse(num) {
-    if (typeof num === "number" && !isNaN(num)) {
-        return -num;
-    }
-    return "User did not provide required data";
- }
+function reverse(num) {
+  if (typeof num === "number" && !isNaN(num)) {
+    return -num;
+  }
+  return "User did not provide required data";
+}
 
- console.log(reverse(8));
- console.log(reverse(-6));
- console.log(reverse(false));
+console.log(reverse(8));
+console.log(reverse(-6));
+console.log(reverse(false));
 
 //task 3
 
@@ -60,28 +60,30 @@ console.log(addThreeCountTimes(NaN, 3));
 // task 4
 
 function kmToM(km) {
-    if (typeof km === "number" && !isNaN(km)) {
-        return km * 1000;
-    }
+  if (typeof km === "number" && !isNaN(km)) {
+    return km * 1000;
+  }
 
-    return errorMessage;   
+  return errorMessage;
 }
 
 function kmToSm(km) {
-    if (typeof km === "number" && !isNaN(km)) {
-        return km * 100000;
-    }
+  if (typeof km === "number" && !isNaN(km)) {
+    return km * 100000;
+  }
 
-    return errorMessage;   
+  return errorMessage;
 }
 
 function getMetric(metric, km, m, sm) {
-    switch(metric) {
-        case "m": return m(km);
-        case "sm":  return sm(km);
-        default: return errorMessage;
-    }
- 
+  switch (metric) {
+    case "m":
+      return m(km);
+    case "sm":
+      return sm(km);
+    default:
+      return errorMessage;
+  }
 }
 
 console.log(getMetric("m", 7, kmToM, kmToSm));
