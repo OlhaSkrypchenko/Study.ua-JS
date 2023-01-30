51211fb95633b2eaf5e6a49b494ca92016216976
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-01-30 21:36:55 +0200

    added solutions to 3 tasks

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-'use strict';
+"use strict";
+
+//task 1-2-3
+
+class User {
+  constructor(name, login, age) {
+    this._name = name;
+    this.login = login;
+    this.age = age;
+  }
+
+  getName(isAdmin) {
+    return isAdmin ? this._name || this.login : "Permission denied";
+  }
+
+  changeName(name, password) {
+    return password === "123"
+      ? `Name Changed From ${this._name} to ${(this._name = name)}`
+      : "Permission denied";
+  }
+}
+
+const user1 = new User("Mike", "MK_18", 18);
+const user2 = new User("", "NRG", 22);
+
+console.log(user1.login);
+console.log(user1.age);
+console.log(user2.login);
+console.log(user2.age);
+
+console.log(user1.getName(true));
+console.log(user2.getName(true));
+console.log(user2.getName(false));
+
+console.log(user1.changeName("Bill", "123"));
+
