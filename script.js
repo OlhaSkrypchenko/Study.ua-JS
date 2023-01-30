c9a66e9f647d99effea5e61b86c3ab8e507a63f8
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-01-30 23:51:04 +0200

    added solutions to 4 and 5 tasks

--- script.js
+++ script.js
@@ -3,20 +3,22 @@
 //task 1-2-3
 
 class User {
+  #name;
+
   constructor(name, login, age) {
-    this._name = name;
+    this.#name = name;
     this.login = login;
     this.age = age;
   }
 
   getName(isAdmin) {
-    return isAdmin ? this._name || this.login : "Permission denied";
+    return isAdmin ? this.#name || this.login : "Permission denied";
   }
 
   changeName(name, password) {
     if (password === "123") {
-      const oldName = this._name;
-      this._name = name;
+      const oldName = this.#name;
+      this.#name = name;
 
       return `Name Changed From ${oldName} to ${name}`;
     }
@@ -38,3 +40,59 @@ console.log(user2.getName(true));
 console.log(user2.getName(false));
 
 console.log(user1.changeName("Bill", "123"));
+
+//task 4
+
+class Admin extends User {
+  #isAdmin;
+
+  constructor(...args) {
+    super(...args);
+    this.#isAdmin = true;
+  }
+
+  getUserName(user) {
+    return user.getName(this.#isAdmin);
+  }
+}
+
+const admin = new Admin();
+
+console.log(admin.getUserName(user1));
+
+// task 5
+
+class User1 {
+  #phone;
+
+  constructor(name, phone) {
+    this.name = name;
+    this.#phone = phone;
+  }
+
+  getPhone(isAdmin) {
+    if (isAdmin) {
+      return this.#phone;
+    }
+
+    const phoneArr = this.#phone.split("");
+
+    const hiddenPhone = phoneArr.map((el, index) => {
+      if (index >= 4 && index <= 9) {
+        return "*";
+      }
+
+      return el;
+    });
+
+    return hiddenPhone.join("");
+  }
+}
+
+const user3 = new User1("Mike", "067-888-88-77");
+const user4 = new User1("Tom", "067-888-88-77");
+
+console.log(user3.getPhone(true));
+console.log(user3.getPhone(false));
+console.log(user4.getPhone(true));
+console.log(user4.getPhone(false));
