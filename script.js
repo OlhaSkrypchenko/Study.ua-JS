e21be974a999698f19ce331c94f8bcc696adfa13
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-02-03 15:36:54 +0200

    added solutions of 1 and 2 tasks

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// task 1
+
+const user = {};
+
+Object.defineProperties(user, {
+  name: {value: 'Ivan', writable: false},
+  age: {value: 20, writable: false},
+  id: {value: 123, configurable: false}
+});
+
+// task 2
+let dataBase = {
+  dbName:'user',
+  dbType: 'MySQL'
+}
+
+Object.freeze(dataBase);
+
+let configurateDB = {
+  token: '123',
+  password: '567',
+  user: 'admin'
+}
+
+Object.seal(configurateDB);
