c977d62ad1a14f6f30a8208b0895f1406e92302c
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-02-03 18:16:52 +0200

    added solution of 4 task

--- script.js
+++ script.js
@@ -2,87 +2,92 @@
 
 // task 1
 
-// const user = {};
+const user = {};
 
-// Object.defineProperties(user, {
-//   name: { value: "Ivan", writable: false },
-//   age: { value: 20, writable: false },
-//   id: { value: 123, configurable: false },
-// });
+Object.defineProperties(user, {
+  name: { value: "Ivan", writable: false },
+  age: { value: 20, writable: false },
+  id: { value: 123, configurable: false },
+});
 
-// // task 2
-// let dataBase = {
-//   dbName: "user",
-//   dbType: "MySQL",
-// };
+// task 2
+let dataBase = {
+  dbName: "user",
+  dbType: "MySQL",
+};
 
-// Object.freeze(dataBase);
+Object.freeze(dataBase);
 
-// let configurateDB = {
-//   token: "123",
-//   password: "567",
-//   user: "admin",
-// };
+let configurateDB = {
+  token: "123",
+  password: "567",
+  user: "admin",
+};
 
-// Object.seal(configurateDB);
+Object.seal(configurateDB);
 
-// // task 3
+// task 3
 
-// let salaries = {
-//   frontend: 2000,
-//   backend: 1500,
-//   design: 1000,
-// };
+let salaries = {
+  frontend: 2000,
+  backend: 1500,
+  design: 1000,
+};
 
-// Object.defineProperty(salaries, "sum", {
-//   get() {
-//     let sum = 0;
-//     for (salary in this) {
-//       sum += this[salary];
-//     }
+Object.defineProperty(salaries, "sum", {
+  get() {
+    let sum = 0;
+    for (salary in this) {
+      sum += this[salary];
+    }
 
-//     return sum;
-//   },
-//   enumerable: false,
-// });
+    return sum;
+  },
+  enumerable: false,
+});
 
-// Object.defineProperty(salaries, "addSalaries", {
-//   set(arr) {
-//     if (Array.isArray(arr)) {
-//       arr.forEach((el) => {
-//         let [key, salaryAmount] = el.split(": ");
-//         salaries[key] = +salaryAmount;
-//       });
-//     }
-//   },
+Object.defineProperty(salaries, "addSalaries", {
+  set(arr) {
+    if (Array.isArray(arr)) {
+      arr.forEach((el) => {
+        let [key, salaryAmount] = el.split(": ");
+        salaries[key] = +salaryAmount;
+      });
+    }
+  },
 
-//   enumerable: false,
-// });
+  enumerable: false,
+});
 
-// salaries.addSalaries = ['frontend: 2500', 'backend: 1750', 'design: 1300', 'manager: 800'];
-// console.log(salaries);
+salaries.addSalaries = [
+  "frontend: 2500",
+  "backend: 1750",
+  "design: 1300",
+  "manager: 800",
+];
+console.log(salaries);
 
-// // task 4
+// task 4
 
-// let user1 = {
-//   name: 'Mike',
-//   surname: 'Davis',
-//   age: 25
-// }
+let user1 = {
+  name: "Mike",
+  surname: "Davis",
+  age: 25,
+};
 
-// Object.defineProperty(user1, 'userInfo', {
-//   get() {
-//     let result = '';
-//     for (key in this) {
-//       result += `${key}: ${this[key]}, `;
-//     }
-  
-//     return result.slice(0, result.length-2);
-//   },
+Object.defineProperty(user1, "userInfo", {
+  get() {
+    let result = "";
+    for (key in this) {
+      result += `${key}: ${this[key]}, `;
+    }
 
-//   enumerable: false
-// })
+    return result.slice(0, result.length - 2);
+  },
 
-// console.log(user1.userInfo);
-// user1.login = 'MK_18';
-// console.log(user1.userInfo);
+  enumerable: false,
+});
+
+console.log(user1.userInfo);
+user1.login = "MK_18";
+console.log(user1.userInfo);
