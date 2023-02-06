3be838db5be61d881ea9865e6873a6ab619cd5a5
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-02-06 17:03:10 +0200

    added solution for 1 and 2 tasks

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 'use strict';
+
+//task 1
+
+// let car = {
+//   type: 'electric',
+//   wheels: 4,
+// }
+
+// let sportCar = {
+//   doors: 2,
+// }
+
+// Object.setPrototypeOf(sportCar, car);
+
+
+// let passengerCar = {
+//   doors: 4,
+// }
+
+// Object.setPrototypeOf(passengerCar, car);
+
+// let toyCar = {
+//   type: 'toy',
+// }
+
+// Object.setPrototypeOf(toyCar, sportCar);
+
+// task 2
+
+let employees = {
+  wallet: {},
+  pay(month, sum) {
+    this.wallet[month] = sum;
+  }
+}
+
+let frontendDeveloper = {
+  wallet: {},
+  name: 'Mike',
+}
+
+Object.setPrototypeOf(frontendDeveloper, employees);
+
+let backendDeveloper = {
+  wallet: {},
+  name: 'Bob',
+}
+
+Object.setPrototypeOf(backendDeveloper, employees);
+backendDeveloper.pay('june', 1500);
+
+console.log(backendDeveloper.wallet.june);
+console.log(frontendDeveloper.wallet.june);
