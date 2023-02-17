da2f5aaa9e296a09404453e34f388c172cdee047
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-02-17 16:23:57 +0200

    added solution for 1 and 2 tasks

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
 "use strict";
 
+// task 1
+
+let url = "https://jsonplaceholder.typicode.com/todos/";
+
+function getData(url) {
+  let request = fetch(url);
+  return request
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(`Error: $(response.status)`);
+    })
+    .then((data) => {
+      return data.filter(item => item.title[0] === 'a');
+    })
+    .catch((error) => console.log(error.message));
+}
+
+getData(url).then(returnData => {console.log(returnData)});
+
+// task 2
+
+async function getData1(url) {
+  try {
+    let request = await fetch(url);
+
+    if (request.ok) {
+      let data = await request.json();
+      return  data.filter(item => item.title[0] === 'a');
+    } else {
+      throw new Error(`Error: $(response.status)`);
+    }
+  } catch(error) {
+    console.log(error.message)
+  }
+}
+
+getData1(url).then(returnData => {
+  console.log(returnData)
+})
