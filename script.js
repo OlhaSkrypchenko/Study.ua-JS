5d1813eb827337b7da778e0d3bffee9d161f0f24
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-02-19 16:31:30 +0200

    added solution of 4 task

--- script.js
+++ script.js
@@ -14,12 +14,14 @@ function getData(url) {
       throw new Error(`Error: $(response.status)`);
     })
     .then((data) => {
-      return data.filter(item => item.title[0] === 'a');
+      return data.filter((item) => item.title[0] === "a");
     })
     .catch((error) => console.log(error.message));
 }
 
-getData(url).then(returnData => {console.log(returnData)});
+getData(url).then((returnData) => {
+  console.log(returnData);
+});
 
 // task 2
 
@@ -29,36 +31,75 @@ async function getData1(url) {
 
     if (request.ok) {
       let data = await request.json();
-      return  data.filter(item => item.title[0] === 'a');
+      return data.filter((item) => item.title[0] === "a");
     } else {
       throw new Error(`Error: $(response.status)`);
     }
-  } catch(error) {
-    console.log(error.message)
+  } catch (error) {
+    console.log(error.message);
   }
 }
 
-getData1(url).then(returnData => {
-  console.log(returnData)
-})
+getData1(url).then((returnData) => {
+  console.log(returnData);
+});
 
+// task 3-4
 
-let titlesA = document.querySelector('.user-a');
-let titlesAB = document.querySelector('.user-ab');
+let titlesA = document.querySelector(".user-a");
+let titlesAB = document.querySelector(".user-ab");
 
-function createUsers(elem, content ='', parent) {
+function createUsers(elem, content = "", parent) {
   let row = document.createElement(elem);
   row.textContent = content;
   parent.append(row);
 }
 
-getData1(url).then(returnData => {
-  returnData.forEach(element => {
-   createUsers('div', JSON.stringify(element), titlesA);
-
-    if(element.title[1] === 'b') {
-      createUsers('div', JSON.stringify(element), titlesAB);
-    }
-   
+function renderDataA() {
+  getData1(url).then((returnData) => {
+    titlesA.innerHTML = "";
+    returnData.forEach((element) => {
+      createUsers("div", JSON.stringify(element), titlesA);
+      titlesAB.innerHTML = "";
+    });
   });
-})
+}
+
+function renderDataAB() {
+  getData1(url).then((returnData) => {
+    titlesAB.innerHTML = "";
+
+    returnData.forEach((element) => {
+      if (element.title.startsWith('ab')) {
+        createUsers("div", JSON.stringify(element), titlesAB);
+
+        titlesA.innerHTML = "";
+      }
+    });
+  });
+}
+
+function renderDataAll() {
+  getData1(url).then((returnData) => {
+    titlesA.innerHTML = "";
+    titlesAB.innerHTML = "";
+
+    returnData.forEach((element) => {
+      createUsers("div", JSON.stringify(element), titlesA);
+
+      if (element.title[1] === "b") {
+        createUsers("div", JSON.stringify(element), titlesAB);
+      }
+    });
+  });
+}
+
+
+const btnA = document.querySelector(".add-a");
+const btnAB = document.querySelector(".add-ab");
+const btnAll = document.querySelector(".add-all");
+
+btnA.addEventListener("click", renderDataA);
+btnAB.addEventListener("click", renderDataAB);
+btnAll.addEventListener("click", renderDataAll);
+
