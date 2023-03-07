db4bb56295bc30ab6f07a934670930e0b2df341b
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-03-07 15:24:03 +0200

    added solutions for all tasks

---
+++ react-app/src/App.js
@@ -0,0 +1,60 @@
+import "./App.css";
+import "./components/hello/Hello";
+import Hello from "./components/hello/Hello";
+
+function App() {
+
+  // task 1
+
+  let usersName = ["Mike", "Bob", "Nikola"];
+
+  let users = {};
+
+  [users.mike, users.bob, users.nikola] = usersName;
+
+  console.log(users);
+
+  // task 2
+
+  let salaries = {
+    Mike: 1500,
+    Bob: 1800,
+    Nikola: 1700,
+  };
+
+  function maxSalaries(salaries) {
+    const arr = Object.entries(salaries);
+    let [name, salary] = ["", 0];
+
+    arr.forEach((elem) => {
+      if (elem[1] > salary) {
+        [name, salary] = elem;
+      }
+    });
+
+    return name;
+  }
+
+  console.log(maxSalaries(salaries));
+
+  //task 3
+
+  const usersThirdTask = { mike: "Mike", bob: "Bob", nikola: "Nikola" };
+
+  let { mike: userMike, bob: userBob, nikola: userNikola } = usersThirdTask;
+
+  console.log(userMike);
+  console.log(userBob);
+  console.log(userNikola);
+
+  // task 4
+
+  return (
+    <div className="App">
+      <Hello />
+      <button>button</button>
+    </div>
+  );
+}
+
+export default App;
