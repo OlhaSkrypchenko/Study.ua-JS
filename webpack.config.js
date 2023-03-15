54342b5fb9f45325fa051623ba227c501c4ece4a
Olha Skrypchenko <olhaskrypchenko@gmail.com>
2023-03-15 17:23:31 +0200

    added webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                },
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    devServer:{
+        watchFiles: path.resolve(__dirname, 'src'),
+        port: 3000
+      },      
+}
